fc00da3f072f0b9a8bde12cf57c0a779812188f7
brianmwevi <mwevibrian@gmail.com>
2022-06-30 15:32:20 +0300

    ft:models -- change name, and adds customer account creation signals

--- sirenapp/models.py
+++ sirenapp/models.py
@@ -1,94 +1,140 @@
 from django.db import models
 from core.settings import AUTH_USER_MODEL
-from django.utils import timezone
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 
-class Account(models.Model):
+class Hospital(models.Model):
+    owner = models.ForeignKey(
+        AUTH_USER_MODEL, related_name='owner', on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, unique=True)
+    location = models.CharField(max_length=255)
+    ambulances = models.ManyToManyField(
+        'Ambulance', related_name='ambulances', blank=True)
+    established_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    doctors = models.ManyToManyField(
+        'Doctor', related_name='doctors', blank=True)
+    reviews = models.ManyToManyField(
+        'Review', related_name='reviews', blank=True)
+    account = models.ForeignKey(
+        'CustomerAccount', blank=True, null=True, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Ambulance(models.Model):
+    availability = models.BooleanField('available', default=False)
+    number_plate = models.CharField(max_length=200, blank=True, null=True)
+    trips = models.ManyToManyField(
+        'Trip', blank=True, related_name='trips')
+
+
+class Doctor(models.Model):
+    first_name = models.CharField(max_length=55)
+    last_name = models.CharField(max_length=55)
+    email = models.EmailField(max_length=255)
+    phone = models.CharField(max_length=255)
+    ambulance = models.ForeignKey(
+        Ambulance, on_delete=models.CASCADE, related_name="doctor_ambulance")
+
+
+class Driver(models.Model):
+    first_name = models.CharField(max_length=55)
+    last_name = models.CharField(max_length=55)
+    email = models.EmailField(max_length=255)
+    phone = models.CharField(max_length=255)
+    ambulance = models.ForeignKey(
+        Ambulance, on_delete=models.CASCADE, related_name="driver_ambulance")
+
+
+class CustomerAccount(models.Model):
     account_holder = models.ForeignKey(
         AUTH_USER_MODEL, on_delete=models.CASCADE)
     account_number = models.PositiveBigIntegerField(
         unique=True, blank=True, null=True)
     balance = models.PositiveIntegerField(default=0)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def generate_account_number():
+        import random
+        import string
+        account_number = "".join(random.choice(string.digits)
+                                 for _ in range(0, 12))
+        return account_number
 
     def __str__(self):
-        return self.account_number
-
-
-class Sender(models.Model):
-    sender = models.ForeignKey(
-        AUTH_USER_MODEL, on_delete=models.CASCADE)
-    account = models.ForeignKey(
-        Account, on_delete=models.CASCADE)
-    account_number = models.PositiveBigIntegerField(
-        unique=True, blank=True, null=True)
-
-    def __str__(self):
-        return self.sender.username
-
-
-class Reciever(models.Model):
-    reciever = models.ForeignKey(
-        AUTH_USER_MODEL, on_delete=models.CASCADE)
-    account = models.ForeignKey(
-        Account, on_delete=models.CASCADE)
-    account_number = models.PositiveBigIntegerField(
-        unique=True, blank=True, null=True)
-
-    def __str__(self):
-        return self.reciever.username
+        return f"{self.account_holder.username}: {self.account_number}"
 
 
 class Transaction(models.Model):
     sender = models.ForeignKey(
-        Sender, on_delete=models.CASCADE)
-    reciever = models.ForeignKey(
-        Reciever, on_delete=models.CASCADE)
-    account_number = models.ForeignKey(Account, on_delete=models.CASCADE)
+        AUTH_USER_MODEL, related_name='sender', on_delete=models.CASCADE)
+    receiver = models.ForeignKey(
+        AUTH_USER_MODEL, related_name='receiver', on_delete=models.CASCADE)
     amount = models.PositiveIntegerField(default=0)
-    trans_date = models.DateTimeField(default=timezone.now)
+    trans_date = models.DateTimeField(auto_now_add=True)
+    completed = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.trans_date
-
-
-class Profile(models.Model):
-    picture = models.ImageField(blank=True, null=True)
-    username = models.CharField(max_length=200, blank=True, null=True)
-    emergency_contact = models.CharField(max_length=200, blank=True, null=True)
-    package = models.ForeignKey('Package', on_delete=models.CASCADE)
-    medical_conditions = models.CharField(
-        max_length=200, blank=True, null=True)
-    account_number = models.ForeignKey('Account', on_delete=models.CASCADE)
-    trip = models.ForeignKey('Trip', on_delete=models.CASCADE)
-    availability = models.BooleanField('available', default=False)
-    plate_number = models.CharField(max_length=200, blank=True, null=True)
-    user = models.OneToOneField(AUTH_USER_MODEL, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.username
+        return f"{self.sender.username} TO {self.receiver.username}"
 
 
 class Package(models.Model):
     CHOICES = [
-        ('regular', 'regular'),
-        ('premium', 'premium'),
+        ('1', 'regular'),
+        ('2', 'premium'),
     ]
     package_choice = models.CharField(
         max_length=200, choices=CHOICES, default="regular")
-    user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE)
+    amount = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return self.package_choice
 
 
 class Trip(models.Model):
-    trip_date = models.DateTimeField(default=timezone.now)
-    driver = models.ForeignKey(
-        AUTH_USER_MODEL, related_name='driver', on_delete=models.CASCADE)
-    fee = models.ForeignKey(
-        'Transaction', related_name='trip_fee', on_delete=models.CASCADE)
     pickup = models.CharField(max_length=255)
     destination = models.CharField(max_length=255)
+    persons = models.PositiveIntegerField(default=3)
+    fee = models.ForeignKey(
+        'Transaction', related_name='trip_fee', on_delete=models.CASCADE)
+    trip_date = models.DateTimeField(auto_now_add=True)
+    completed = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.trip_date
+        return f"{self.pickup} TO {self.destination}"
+
+
+class Review(models.Model):
+    user = models.ForeignKey(
+        AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="reviews")
+    hospital = models.ForeignKey(
+        Hospital, on_delete=models.CASCADE, related_name="hospital_reviews", null=True, blank=True)
+    ambulance = models.ForeignKey(
+        Ambulance, on_delete=models.CASCADE, related_name="ambulance_reviews", null=True, blank=True)
+    content = models.TextField(blank=True, null=True)
+    rating = models.PositiveIntegerField(default=1)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+
+# Custom signals and methods
+@receiver(post_save, sender=Hospital)
+@receiver(post_save, sender=AUTH_USER_MODEL)
+def create_fund_account(sender, instance, created, **kwargs):
+    if created:
+        account_number = CustomerAccount.generate_account_number()
+        try:
+            account = CustomerAccount.objects.get(
+                account_number=account_number)
+            create_fund_account(sender, instance, created, **kwargs)
+        except CustomerAccount.DoesNotExist:
+            account_holder = instance
+            if isinstance(instance, Hospital):
+                account_holder = instance.owner
+            account = CustomerAccount.objects.create(
+                account_number=account_number, account_holder=account_holder)
+            instance.account = account
+            instance.save()
