abff7717c7cd760b541462ecbdc28b68aba14cc4
Aliyo254 <alinurali254@gmail.com>
2022-07-04 12:38:55 +0300

    Auth api2

--- api/serializers.py
+++ api/serializers.py
@@ -5,7 +5,7 @@ from accounts.models import User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'first_name', 'email', 'password']
+        fields = ['id', 'email','first_name','last_name','phone', 'password']
         extra_kwargs = {
             'password': {'write_only': True}
         }
