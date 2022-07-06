b9e220c972643141e274cb9be963855dd437501c
brianmwevi <mwevibrian@gmail.com>
2022-07-06 12:03:19 +0300

    ch:admin dashboard -- register emergency and patient profile models

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import User
+from .models import EmergencyContact, PatientProfile, User
 
 # Register your models here.
 admin.site.register(User)
+admin.site.register(EmergencyContact)
+admin.site.register(PatientProfile)
