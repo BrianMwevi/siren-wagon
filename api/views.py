14a860bb13d831face3f788e1adb4043371316f3
brianmwevi <mwevibrian@gmail.com>
2022-07-06 12:09:16 +0300

    ft:logout -- add logout jwt apiview class

--- api/views.py
+++ api/views.py
@@ -78,3 +78,17 @@ class DriverViewset(viewsets.ModelViewSet):
 class PackageViewset(viewsets.ModelViewSet):
     serializer_class = PackageSerializer
     queryset = Package.objects.all()
+
+
+class LogoutView(views.APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh_token"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception as e:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
