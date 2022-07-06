d43191b3948ebd6f484fdb1bec23edd0934c04ba
brianmwevi <mwevibrian@gmail.com>
2022-07-06 12:08:33 +0300

    ft:package -- add modelviewset class

--- api/views.py
+++ api/views.py
@@ -1,22 +1,20 @@
-import datetime
-import jwt
-from .serializers import UserSerializer
 from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.response import Response
-from rest_framework.views import APIView
-from django.db.models import Q
-from sirenapp.models import CustomerAccount, Hospital, Transaction, Trip, Review, Doctor, Ambulance, Driver
-from accounts.models import User
-from api.serializers import UserSerializer, HospitalSerializer, ReviewSerializer, TransactionSerializer, TripSerializer, DoctorSerializer, AmbulanceSerializer, DriverSerializer
+from rest_framework import serializers, viewsets, generics, views
+from rest_framework import status
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
-from rest_framework import viewsets, generics
-from rest_framework import serializers
+
+from rest_framework_simplejwt.tokens import RefreshToken
 
 
-# class UserViewset(viewsets.ModelViewSet):
-#     queryset = User.objects.all()
-#     serializer_class = UserSerializer
+from django.db.models import Q
 
+from sirenapp.models import CustomerAccount, Hospital, Package, Transaction, Trip, Review, Doctor, Ambulance, Driver
+from accounts.models import User, PatientProfile, EmergencyContact
+from api.serializers import EmergencyContactSerializer, PackageSerializer, HospitalSerializer, ReviewSerializer, TransactionSerializer, TripSerializer, DoctorSerializer, AmbulanceSerializer, DriverSerializer, PatientProfileSerializer, UserSerializer
+
+
+# Create your views here.
 
 class TransactionsView(generics.ListCreateAPIView):
     """The view class for creating and retrieving transactions only. One can't edit/change an existing transaction."""
@@ -77,39 +75,6 @@ class DriverViewset(viewsets.ModelViewSet):
     queryset = Driver.objects.all()
 
 
-class RegisterView(APIView):
-    def post(self, request):
-        serializer = UserSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data)
-
-
-class LoginView(APIView):
-    def post(self, request):
-        email = request.data['email']
-        password = request.data['password']
-        user = User.objects.filter(email=email).first()
-        if user is None:
-            raise AuthenticationFailed('User not found!')
-        if not user.check_password(password):
-            raise AuthenticationFailed('Incorrect password!')
-        payload = {
-            'id': user.id,
-            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=60),
-            'iat': datetime.datetime.utcnow()
-        }
-        token = jwt.encode(payload, 'secret',
-                           algorithm='HS256').decode('utf-8')
-        response = Response()
-        response.set_cookie(key='jwt', value=token, httponly=True)
-        response.data = {'jwt': token}
-        return response
-
-
-class LogoutView(APIView):
-    def post(self, request):
-        response = Response()
-        response.delete_cookie('jwt')
-        response.data = {'message': 'success'}
-        return response
+class PackageViewset(viewsets.ModelViewSet):
+    serializer_class = PackageSerializer
+    queryset = Package.objects.all()
