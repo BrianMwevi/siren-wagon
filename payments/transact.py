f0c2547a8c751800627867e89c8015ced99e32b4
brianmwevi <mwevibrian@gmail.com>
2022-07-11 02:03:05 +0300

    fix:mpesa -- user can initiate payments

--- payments/transact.py
+++ payments/transact.py
@@ -11,42 +11,29 @@ def initiate_transaction(sender, receiver, amount, message):
     token, expired = DarajaToken.get_credentials()
     if expired:
         new_token, expiry = request_new_token()
-        print("New token: ", new_token, expiry)
         updated_token = token.update_token(new_token)
         token = updated_token
     headers = {
+        "Content-Type": "application/json",
         'Authorization': f'Bearer {token.token}'
 
     }
 
     payload = {
-        "BusinessShortCode": 174379,
-        "Password": config("DARAJA_PASSWORD"),
-        "Timestamp": "20220710230712",
+        "BusinessShortCode": config("BUSINESS_SHORT_CODE"),
+        "Password": config('DARAJA_PASSWORD'),
+        "Timestamp": "20220710132848",
         "TransactionType": "CustomerPayBillOnline",
         "Amount": 1,
-        "PartyA": 254708374149,
-        "PartyB": 600000,
-        "PhoneNumber": 254708374149,
+        "PartyA": sender.phone,
+        "PartyB": config("BUSINESS_SHORT_CODE"),
+        "PhoneNumber": receiver.account_holder.phone,
         "CallBackURL": "https://mydomain.com/path",
-        "AccountReference": "CompanyXLTD",
-        "TransactionDesc": "Payment of X"
+        "AccountReference": "Siren Wagon",
+        "TransactionDesc": message
     }
-    # {
-    #     "BusinessShortCode": 174379,
-    #     "Password": config("DARAJA_PASSWORD"),
-    #     "Timestamp": "20220710132848",
-    #     "TransactionType": "CustomerPayBillOnline",
-    #     "Amount": amount,
-    #     "PartyA": sender.phone,
-    #     "PartyB": 174379,
-    #     "PhoneNumber": receiver.account_holder.phone,
-    #     "CallBackURL": "https://mydomain.com/path",
-    #     "AccountReference": "Siren Wagon",
-    #     "TransactionDesc": message
-    # }
 
     response = requests.request(
-        'POST', config('MPESA_EXPRESS'), headers=headers, data=payload)
-    print(response.text.encode('utf-8'))
+        'POST', config('MPESA_EXPRESS'), headers=headers, json=payload)
+    print("\n", json.loads(response.text.encode('utf-8')), "\n")
     return json.loads(response.text.encode('utf-8'))
