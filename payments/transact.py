8cc726b6d334c489bdbf50866805438e35f4e2c2
brianmwevi <mwevibrian@gmail.com>
2022-07-11 18:31:15 +0300

    ft:token -- autocreates token if not exists

--- payments/transact.py
+++ payments/transact.py
@@ -1,6 +1,6 @@
 
-from cmath import exp
 import requests
+from datetime import datetime
 from payments.models import DarajaToken
 from payments.daraja_auth import request_new_token
 from decouple import config
@@ -9,27 +9,31 @@ import json
 
 def initiate_transaction(sender, receiver, amount, message):
     token, expired = DarajaToken.get_credentials()
-    if expired:
+    if not token or expired:
         new_token, expiry = request_new_token()
-        updated_token = token.update_token(new_token)
-        token = updated_token
+        if expired:
+            updated_token = token.update_token(new_token)
+            token = updated_token
+        else:
+            token = DarajaToken.objects.create(token=new_token)
     headers = {
         "Content-Type": "application/json",
         'Authorization': f'Bearer {token.token}'
 
     }
+    timestamp = datetime.now().strftime("%Y-%m-%d%H:%M:%S")
 
     payload = {
         "BusinessShortCode": config("BUSINESS_SHORT_CODE"),
         "Password": config('DARAJA_PASSWORD'),
         "Timestamp": "20220710132848",
         "TransactionType": "CustomerPayBillOnline",
-        "Amount": 1,
+        "Amount": amount,
         "PartyA": sender.phone,
         "PartyB": config("BUSINESS_SHORT_CODE"),
-        "PhoneNumber": receiver.account_holder.phone,
+        "PhoneNumber": sender.phone,
         "CallBackURL": "https://mydomain.com/path",
-        "AccountReference": "Siren Wagon",
+        "AccountReference": receiver.account_number,
         "TransactionDesc": message
     }
 
