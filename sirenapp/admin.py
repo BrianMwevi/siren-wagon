35fe82b1fa81bf0b563b152d8f0d6e914b93ae03
brianmwevi <mwevibrian@gmail.com>
2022-07-12 23:04:16 +0300

    fix:doctor and driver -- remove models from admin

--- sirenapp/admin.py
+++ sirenapp/admin.py
@@ -1,11 +1,9 @@
 from django.contrib import admin
-from .models import CustomerAccount, Trip, Package, Transaction, Hospital, Driver, Doctor, Ambulance
+from .models import CustomerAccount, Trip, Package, Transaction, Hospital, Ambulance
 
 admin.site.register(CustomerAccount)
 admin.site.register(Trip)
 admin.site.register(Package)
 admin.site.register(Transaction)
-admin.site.register(Doctor)
 admin.site.register(Ambulance)
-admin.site.register(Driver)
 admin.site.register(Hospital)
